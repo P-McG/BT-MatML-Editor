0f53075397329074ab552cb726d5bde274ed5adb
P-McG <101891191+P-McG@users.noreply.github.com>
2022-09-12 16:31:48 +0000

    Deploying to gh-pages from  @ a7106822dc014e01bf410a55344010cd5b613e62 🚀

--- search/functions_f.js
+++ search/functions_f.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['reestabilishtreectrlsizer_0',['reEstabilishTreeCtrlSizer',['../classbellshire_1_1_material_frame.html#a083e10d89bffe80a2aba029e35ab1507',1,'bellshire::MaterialFrame']]],
   ['relationship_5fgui_1',['Relationship_GUI',['../classbellshire_1_1_relationship___g_u_i.html#a7aeac30f2926a56253390c9835f00487',1,'bellshire::Relationship_GUI::Relationship_GUI()'],['../classbellshire_1_1_relationship___g_u_i.html#a9b6c024109dbb100459c855d3552daca',1,'bellshire::Relationship_GUI::Relationship_GUI(wxWindow *parent)']]],
-  ['replaceidwithuuid_2',['ReplaceIDwithUuid',['../classbellshire_1_1_material_frame.html#af5382fe6ad5ba59a2899dc9bdf111dfe',1,'bellshire::MaterialFrame']]],
+  ['replaceidwithuuid_2',['ReplaceIDwithUuid',['../classbellshire_1_1_material___g_u_i___base.html#ab29cdc2a573adf302568bd8a638b42e2',1,'bellshire::Material_GUI_Base']]],
   ['result_5fgui_3',['Result_GUI',['../classbellshire_1_1_result___g_u_i.html#a942fe6e13272042445bfe76f17e2aeb7',1,'bellshire::Result_GUI::Result_GUI()'],['../classbellshire_1_1_result___g_u_i.html#a7f559f47a701f5418a48943f630a0817',1,'bellshire::Result_GUI::Result_GUI(wxWindow *parent)']]]
 ];
